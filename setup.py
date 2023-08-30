d4663bf34b0a533e45ce0fcf22c150290e3fc047
junaid-o <junaidmohd795@gmail.com>
2023-08-31 05:07:34 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="BraTS_Segmentation",
+    version="0.2",
+    packages=find_packages(where='.'),    
+    author="junaid-o",    
+    description="BraTS2020 3D Segmentation Project",
+    )
